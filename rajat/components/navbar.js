962f4b9b571518df9c4e33bba8222a3416c488a7
runforest881 <yyrajat881@gmail.com>
2022-07-20 15:28:58 +0530

    lla

---
+++ rajat/components/navbar.js
@@ -0,0 +1,24 @@
+let nav__bar = ()=>{
+    return `
+            <div class="cont_ner">
+            <a class="navbar_brand_click" href="index.html"><img src="https://cdn-m.timecamp.com/img/greenbranding/colorLogo.svg"></a>
+            <div id="dashboard_set">
+            <ul class="navbar_nav">
+            <li class="navbar_ul_li"><a href='#'>Features</a></li>
+            <li class="navbar_ul_li"><a href='#'>Pricing</a></li>
+            <li class="navbar_ul_li"><a href='#'>Integration</a></li>
+            <li class="navbar_ul_li"><a href='#'>Blog</a></li>
+            </ul>
+
+            <ul class="navbar_nav">
+            <li class="navbar_ul_li"><a href='#'>Book a Demo</a></li>
+            <li class="navbar_ul_li"><a href='#'>Sign in</a></li>
+            <li class="navbar_ul_li" id="Zbutton" ><a href='#' style="color:#fff">Go to App</a></li>           
+            </ul>
+            </div>
+
+            </div>
+            
+            `
+}
+export default nav__bar;
