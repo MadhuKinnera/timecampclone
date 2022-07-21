c2206bf964559f9bb00e58f90962343a8e2c1672
runforest881 <yyrajat881@gmail.com>
2022-07-22 03:02:49 +0530

    m

---
+++ rajat/components/footer.js
@@ -0,0 +1,154 @@
+let foot_er = ()=>{
+   return `
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <p>Products</p>
+                        <ul>
+                            <li>
+                                <a href="#">Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Integration</a>
+                            </li>
+                            <li>
+                                <a href="#">Application</a>
+                            </li>
+                            <li>
+                                <a href="#">Product update</a>
+                            </li>
+                            <li>
+                                <a href="#">Customer Stories</a>
+                            </li>
+                            <li>
+                                <a href="#">Self-hosting</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Support</p>
+                        <ul>
+                            <li>
+                                <a href="#">Support</a>
+                            </li>
+                            <li>
+                                <a href="#">Knowledge base</a>
+                            </li>
+                            <li>
+                                <a href="#">Developers API</a>
+                            </li>
+                            <li>
+                                <a href="#">Server Status</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Integration</p>
+                        <ul>
+                            <li>
+                                <a href="#">Trello</a>
+                            </li>
+                            <li>
+                                <a href="#">Monday.com</a>
+                            </li>
+                            <li>
+                                <a href="#">Google Calendar</a>
+                            </li>
+                            <li>
+                                <a href="#">Asana</a>
+                            </li>
+                            <li>
+                                <a href="#">Jira</a>
+                            </li>
+                            <li>
+                                <a href="#">All integrations</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Resources</p>
+                        <ul>
+                            <li>
+                                <a href="#">Contact</a>
+                            </li>
+                            <li>
+                                <a href="#">HeySpace</a>
+                            </li>
+                            <li>
+                                <a href="#">About us</a>
+                            </li>
+                            <li>
+                                <a href="#">Press</a>
+                            </li>
+                            <li>
+                                <a href="#">Become a Partner</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Calculators</p>
+                        <ul>
+                            <li>
+                                <a href="#">Profit Margin Calculator</a>
+                            </li>
+                            <li>
+                                <a href="#">Online Timer</a>
+                            </li>
+                            <li>
+                                <a href="#">Overtime Calculator</a>
+                            </li>
+                            <li>
+                                <a href="#">ROI Calculator</a>
+                            </li>
+                            <li>
+                                <a href="#">Time Card Calculator</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Legal</p>
+                        <ul>
+                            <li>
+                                <a href="#">Terms</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy</a>
+                            </li>
+                            <li>
+                                <a href="#">Security</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <a href="#">
+                            <img src="" alt="Error">
+                        </a>
+                        <a href="#">
+                            <img src="" alt="Error">
+                        </a>
+                        <a href="#">
+                            <img src="" alt="Error">
+                        </a>
+                    </div>
+                    <div>
+                        <p>
+                            <a href="#">Click here to learn more</a>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <!-- <button></button> -->
+                    <div>
+                        <a href="#">Polish</a>
+                        <a href="#">Spanish</a>
+                        <a href="#">Portugese</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+export default foot_er;
--- rajat/scripts/index.js
+++ rajat/scripts/index.js
@@ -1,3 +1,7 @@
 import nav__bar from "../components/navbar.js"
 
-document.querySelector(".nav_bar_section").innerHTML = nav__bar()
+import foot_er from "../components/footer.js"
+
+document.querySelector(".nav_bar_section").innerHTML = nav__bar();
+
+document.getElementById("last_part_footer").innerHTML = foot_er();
