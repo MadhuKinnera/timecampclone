2d2a6b334e751465a0d72603cb44e964bb86463f
runforest881 <yyrajat881@gmail.com>
2022-07-21 12:54:07 +0530

    kkk

--- rajat/components/navbar.js
+++ rajat/components/navbar.js
@@ -4,15 +4,17 @@ let nav__bar = ()=>{
             <a class="navbar_brand_click" href="index.html"><img src="https://cdn-m.timecamp.com/img/greenbranding/colorLogo.svg"></a>
             <div id="dashboard_set">
             <ul class="navbar_nav">
-            <li class="navbar_ul_li"><a href='#'>Features</a></li>
-            <li class="navbar_ul_li"><a href='#'>Pricing</a></li>
-            <li class="navbar_ul_li"><a href='#'>Integration</a></li>
-            <li class="navbar_ul_li"><a href='#'>Blog</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Features</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Pricing</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Integration</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Blog</a></li>
             </ul>
 
             <ul class="navbar_nav">
-            <li class="navbar_ul_li"><a href='#'>Book a Demo</a></li>
-            <li class="navbar_ul_li"><a href='#'>Sign in</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Book a Demo</a></li>
+            <li class="vl"></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Sign in</a></li>
+            
             <li class="navbar_ul_li" id="Zbutton" ><a href='#' style="color:#fff">Go to App</a></li>           
             </ul>
             </div>
