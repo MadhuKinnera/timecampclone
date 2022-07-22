3ff2e4ec8fc2fdfff0ddd4f45fc00bbae1066076
runforest881 <yyrajat881@gmail.com>
2022-07-22 14:39:58 +0530

    oo

--- rajat/components/footer.js
+++ rajat/components/footer.js
@@ -1,153 +1,98 @@
 let foot_er = ()=>{
    return `
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <p>Products</p>
-                        <ul>
-                            <li>
-                                <a href="#">Features</a>
-                            </li>
-                            <li>
-                                <a href="#">Integration</a>
-                            </li>
-                            <li>
-                                <a href="#">Application</a>
-                            </li>
-                            <li>
-                                <a href="#">Product update</a>
-                            </li>
-                            <li>
-                                <a href="#">Customer Stories</a>
-                            </li>
-                            <li>
-                                <a href="#">Self-hosting</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p>Support</p>
-                        <ul>
-                            <li>
-                                <a href="#">Support</a>
-                            </li>
-                            <li>
-                                <a href="#">Knowledge base</a>
-                            </li>
-                            <li>
-                                <a href="#">Developers API</a>
-                            </li>
-                            <li>
-                                <a href="#">Server Status</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p>Integration</p>
-                        <ul>
-                            <li>
-                                <a href="#">Trello</a>
-                            </li>
-                            <li>
-                                <a href="#">Monday.com</a>
-                            </li>
-                            <li>
-                                <a href="#">Google Calendar</a>
-                            </li>
-                            <li>
-                                <a href="#">Asana</a>
-                            </li>
-                            <li>
-                                <a href="#">Jira</a>
-                            </li>
-                            <li>
-                                <a href="#">All integrations</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p>Resources</p>
-                        <ul>
-                            <li>
-                                <a href="#">Contact</a>
-                            </li>
-                            <li>
-                                <a href="#">HeySpace</a>
-                            </li>
-                            <li>
-                                <a href="#">About us</a>
-                            </li>
-                            <li>
-                                <a href="#">Press</a>
-                            </li>
-                            <li>
-                                <a href="#">Become a Partner</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p>Calculators</p>
-                        <ul>
-                            <li>
-                                <a href="#">Profit Margin Calculator</a>
-                            </li>
-                            <li>
-                                <a href="#">Online Timer</a>
-                            </li>
-                            <li>
-                                <a href="#">Overtime Calculator</a>
-                            </li>
-                            <li>
-                                <a href="#">ROI Calculator</a>
-                            </li>
-                            <li>
-                                <a href="#">Time Card Calculator</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p>Legal</p>
-                        <ul>
-                            <li>
-                                <a href="#">Terms</a>
-                            </li>
-                            <li>
-                                <a href="#">Privacy</a>
-                            </li>
-                            <li>
-                                <a href="#">Security</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <a href="#">
-                            <img src="" alt="Error">
-                        </a>
-                        <a href="#">
-                            <img src="" alt="Error">
-                        </a>
-                        <a href="#">
-                            <img src="" alt="Error">
-                        </a>
-                    </div>
-                    <div>
-                        <p>
-                            <a href="#">Click here to learn more</a>
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <!-- <button></button> -->
-                    <div>
-                        <a href="#">Polish</a>
-                        <a href="#">Spanish</a>
-                        <a href="#">Portugese</a>
-                    </div>
+   <div class="containerfvb">
+            <div class="deeper">
+                <div class="deeper1"><p>Product</p></div>
+                <div class="s101">
+                    <ul link="#1b1b20">
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">Integrations</a></li>
+                        <li><a href="#">Applications</a></li>
+                        <li><a href="#">Product updates</a></li>
+                        <li><a href="#">Customer Stories</a></li>
+                        <li><a href="#">Self-hosting</a></li>
+                    </ul>
                 </div>
             </div>
+            <div class="deeper">
+                <div class="deeper1"><p>Support</p></div>
+                <div class="s101">
+                    <ul>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Knowledge base</a></li>
+                        <li><a href="#">Developers API</a></li>
+                        <li><a href="#">Server Status</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="deeper">
+                <div class="deeper1"><p>Integrations</p></div>
+                <div class="s101">
+                    <ul>
+                        <li><a href="#">Trello</a></li>
+                        <li><a href="#">Monday.com</a></li>
+                        <li><a href="#">Google Calendar</a></li>
+                        <li><a href="#">Asana</a></li>
+                        <li><a href="#">Jira</a></li>
+                        <li><a href="#">All integrations</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="deeper">
+                <div class="deeper1"><p>Resources</p></div>
+                <div class="s101">
+                    <ul>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">HeySpace</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Press</a></li>
+                        <li><a href="#">Become a Partner</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="deeper">
+                <div class="deeper1"><p>Calculators</p></div>
+                <div class="s101">
+                    <ul>
+                        <li><a href="#">Profit Margin</a></li>
+                        <li><a href="#">Online Timer</a></li>
+                        <li><a href="#">Overtime</a></li>
+                        <li><a href="#">ROI Calculator</a></li>
+                        <li><a href="#">Time Card</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="deeper">
+                <div class="deeper1"><p>Legal</p></div>
+                <div class="s101">
+                    <ul>
+                        <li><a href="#">Terms</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Security</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="under_container">
+            
+                <div class="under_container202">
+                    
+                    <div class="under_container2002">
+                        
+                            <div>
+                                <img src="./img/facebook.svg" alt="Error">
+                            </div>
+                            <div>
+                                <img src="./img/linkedin.svg" alt="Error">
+                            </div>
+                            <div>
+                                <img src="./img/twitter.svg" alt="Error">
+                            </div>
+                   
+                    </div>
+                
+                    <div class="under_container1111"><p>We use cookies.<a href="#">Click here to learn more</a></p></div>
+                </div>
         </div>
     `
 }
