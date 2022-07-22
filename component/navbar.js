e989e899cec896bf2238b16d09376fdfa42d8ef5
pooja rathod <poojachavan8669@gmail.com>
2022-07-22 23:30:47 +0530

    added the final page files

---
+++ component/navbar.js
@@ -0,0 +1,24 @@
+let nav__bar = ()=>{
+    return `
+            <div class="cont_ner">
+            <a class="navbar_brand_click" href="index.html"><img src="https://cdn-m.timecamp.com/img/greenbranding/colorLogo.svg"></a>
+            <div id="dashboard_set">
+            <ul class="navbar_nav">
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Features</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Pricing</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Integration</a></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Blog</a></li>
+            </ul>
+            <ul class="navbar_nav">
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Book a Demo</a></li>
+            <li class="vl"></li>
+            <li class="navbar_ul_li"><a href='#' style="color:#1b1b20">Sign in</a></li>
+            
+            <li class="navbar_ul_li" id="Zbutton" ><a href='#' style="color:#fff">Go to App</a></li>           
+            </ul>
+            </div>
+            </div>
+            
+            `
+}
+export default nav__bar;
---
+++ script/integration.js
@@ -0,0 +1,7 @@
+import nav__bar from "../component/navbar.js"
+
+import foot_er from "../component/footer.js"
+
+document.querySelector(".nav_bar_section").innerHTML = nav__bar();
+
+document.getElementById("last_part_footer").innerHTML = foot_er();
